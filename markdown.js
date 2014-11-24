2ce093a40045bd78b2c51a46737b57bc41f98adf
beccapaige <rebeccapaige1216@gmail.com>
2014-11-24 15:57:13 -0500

    Refactored markdown.js

--- markdown.js
+++ markdown.js
@@ -1,12 +1,12 @@
 widget = function(){
-	function appendHtml(html){
-		document.getElementById("preview").innerHTML=html;
-	};
-
 	document.getElementById("source").onkeyup=function(e){
 		var textarea = e.target;
 		appendHtml(markdown.toHTML(textarea.value));
 	};
+};
+
+function appendHtml(html){
+	document.getElementById("preview").innerHTML=html;
 };
 
 window.addEventListener('load', widget);
