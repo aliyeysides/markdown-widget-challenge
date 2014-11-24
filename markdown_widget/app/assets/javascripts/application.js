f263c879c741675c7dcddde644a4852eee63147e
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-24 15:23:11 -0500

    Working solution

--- markdown_widget/app/assets/javascripts/application.js
+++ markdown_widget/app/assets/javascripts/application.js
@@ -1,70 +1,40 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-
-
 $(document).ready(function(){
 
   $('#source').bind('keyup', function(event) {
-    var source = $('#source').val();
-    var isItalic = false;
-    var new_source = [];
-    split_source = source.split("");
+    $('#source').bind('keypress', function(e){
+      if(e.keyCode==32){
+         // user has pressed backspace
+        var source = $('#source').val();
+        var isItalic = false;
+        var isBold = false
+        var new_source = [];
+        split_source = source.split(" ")
+        console.log("1" + split_source)
+        for (word in split_source){
+          if (split_source[word] == '*' && isItalic==false){
+              isItalic = true;
+              new_source.push('<i>')
+          } else if (split_source[word]=='*' && isItalic==true){
+              isItalic = false;
+              new_source.push('</i>');
 
-    for (letter in split_source){
-      if (split_source[letter] == '*' && isItalic==false){
-          isItalic = true;
-          new_source.push('<i>')
-      } else if (split_source[letter]=='*' && isItalic==true){
-          isItalic = false;
-          new_source.push('</i>');
+          } 
+          else if (split_source[word]=='**' && isBold==false){
+              isBold = true;
+              new_source.push('<strong>');
 
-      } else {
-          new_source.push(split_source[letter])
+          } 
+          else if (split_source[word]=='**' && isBold==true){
+              isBold = false;
+              new_source.push('</strong>');
+
+          } else {
+              new_source.push(split_source[word])
+          }
+        }
+          console.log("2" + new_source)
+          $('#preview').html(new_source.join(" "))
       }
-    }
-    console.log(new_source.join(''))
-    $('#preview').html(new_source.join(''))
-
-    function italic(source){
-
-      if (isItalic == false){
-        source = source.replace("*", "<i>")
-        isItalic = true
-        $('#preview').html(source)
-      }
-      else
-        source = source.replace("*", "</i>")
-        isItalic = false
-        $('#preview').html(source)
-    }
-
-
-
-    // var style = null;
-    // for (letter in split_source){
-    //   if (split_source[letter] == '*' || split_source[letter] == '_'){
-    //     style = 'italic'
-    //   }
-
-      // if(split_source[letter+1] =='*' || split_source[letter+1] =='_'{
-      //   style = 2
-      // }
-    // }
-
-    // $('#preview').html(source)
+    });
   })
 })
