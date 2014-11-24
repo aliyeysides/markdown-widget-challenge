857e767dcd867aa997a703f25dfd624a74bd3ae5
beccapaige <rebeccapaige1216@gmail.com>
2014-11-24 12:11:50 -0500

    Completed release 0

--- markdown.js
+++ markdown.js
@@ -1,13 +1,9 @@
 widget = function(){
 	function appendHtml(html){
-		// var character = String.fromCharCode(str);
 		document.getElementById("preview").innerHTML=html;
 	};
 
-
 	document.getElementById("source").onkeyup=function(e){
-		console.log(e);
-		// var str = e.keyCode;
 		var textarea = e.target;
 		appendHtml(textarea.value);
 	};
