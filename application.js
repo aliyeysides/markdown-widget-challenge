abe3c8212902ceaabf309e316ff160d39878b3b4
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-24 23:00:49 -0500

    remove console logs

--- application.js
+++ application.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
   $("#source-input").keyup(function(){
     var sourceInput = $(this).val();
-    // console.log(sourceInput);
     array = sourceInput.split("\n");
     string = []
     for(var j = 0; j < array.length; j++){
-      console.log(array[j])
       string.push(new StringFormatter(array[j]).logic());
     }
     $('#preview').html(string.join("\n"));
